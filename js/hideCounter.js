b0315bac37b02d5457e79fb3c62317e790c118a4
Mateusz <matix24@gmail.com>
2018-06-19 10:17:45 +0200

    add guest counter

---
+++ js/hideCounter.js
@@ -0,0 +1,8 @@
+function hideCounter() {
+    var x = document.getElementById("hiddenCounter");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
